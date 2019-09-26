ab3ef0788bbe94b30a94d4496da0abbf22e27859
Zeuhx <cedzhg@gmail.com>
2019-09-26 11:13:29 +0200

    Commit_1

---
+++ src/dbms_lyz/DBManager.java
@@ -0,0 +1,43 @@
+package dbms_lyz;
+
+import java.util.List;
+
+public class DBManager {
+	
+	/** Constructeur privé */
+	private DBManager(){}
+	
+	/** Instance unique non préinitialisée */
+	private static DBManager INSTANCE = null ;
+	
+    /** Point d'accès pour l'instance unique du singleton */
+    public static DBManager getInstance(){           
+        if (INSTANCE == null){   
+        	INSTANCE = new DBManager(); 
+        }
+        return INSTANCE;
+    }
+	
+	public static void init() {
+		DBDef.init();
+	}
+	
+	public static void finish() {
+		DBDef.finish();
+	}
+	
+	public void processCommand() {
+		
+	}
+	
+	public RelDef createRelation(String nomRelation, int nombre, List<String> typeCol) {
+		RelDef rd = null ;
+		return(rd);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/dbms_lyz/RelDef.java
@@ -0,0 +1,24 @@
+package dbms_lyz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * @author Le Marcel, Yu Willy, Zhang Cedric
+ * Garde les infos de schema d'une relation
+ *
+ */
+public class RelDef {
+	private String nomRelation ; 
+	private int nbCol ;
+	private List<String> typeCol ; 
+	
+	public RelDef(String nomRelation, int nbCol) {
+		this.nomRelation = nomRelation ;
+		this.nbCol = nbCol ;
+		this.typeCol = new ArrayList<String>() ;
+	}
+	
+}
+
