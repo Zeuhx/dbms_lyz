f52f413466a4cc5b39b222739e6a9091fb2b0960
Zeuhx <cedzhg@gmail.com>
2019-09-26 19:16:52 +0200

    Fini TP1

--- src/dbms_lyz/DiskManager.java
+++ src/dbms_lyz/DiskManager.java
@@ -1,5 +1,17 @@
 package dbms_lyz;
 
 public class DiskManager {
+	/** Constructeur priv�sSs */
+	private DiskManager(){}
 
+	/** Instance unique non préinitialisée */
+	private static DiskManager INSTANCE = null ;
+
+	/** Point d'accès pour l'instance unique du singleton */
+	public static DiskManager getInstance(){           
+		if (INSTANCE == null){   
+			INSTANCE = new DiskManager(); 
+		}
+		return INSTANCE;
+	}
 }
